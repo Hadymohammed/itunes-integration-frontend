4d3588b970859251e7eca06ccda96bc9a72af322
Abdelhady <hadymohamed2002@gmail.com>
2024-04-30 03:24:25 +0300

    add: itunes search api link

--- src/components/sideMenu.tsx
+++ src/components/sideMenu.tsx
@@ -21,15 +21,11 @@ export default function SideMenu ({collapsed,setCollapsed}:{collapsed:boolean,se
   };
   return (
     <Sidebar style={styles.sideBarHeight} collapsed={collapsed} width="270px">
-      {/* <div style={{cursor:"pointer",zIndex:"1",position:"absolute",right:"18px", margin:"10px"}}>
-        <div onClick={onClickMenuIcon}>
-          <AiOutlineMenu style={{height:"25px",width:"25px"}}/>
-        </div>
-      </div> */}
       <div className={`flex ${!collapsed? "justify-between items-center" : "justify-center" }  p-2`}>
-        {!collapsed? <img src="/itunesLogo.png" alt="logo" className="w-32 ml-6"/> :null}
+        {!collapsed? <a target="_blanck" href="https://developer.apple.com/library/archive/documentation/AudioVideo/Conceptual/iTuneSearchAPI/index.html">
+          <img src="/itunesLogo.png" alt="logo" className="w-32 ml-6"/> </a>:null}
         <div onClick={onClickMenuIcon}>
-          <AiOutlineMenu style={{height:"25px",width:"25px"}}/>
+          <AiOutlineMenu style={{height:"25px",width:"25px"}} className="cursor-pointer"/>
         </div>
       </div>
       <Menu>
@@ -38,14 +34,14 @@ export default function SideMenu ({collapsed,setCollapsed}:{collapsed:boolean,se
         <MenuItem icon={<FaStar/>} className="mt-3 md-3 font-medium text-lg">Favourites</MenuItem>
         <MenuItem icon={<FaClock/>} className="mt-3 md-3 font-medium text-lg">Recent</MenuItem>
         
-        <hr className="w-full h-0.5 bg-red-700"/>
+        <hr className="w-11/12 h-0.5 bg-red-700"/>
           
         <MenuItem icon={<FaCog />} className="mt-3 md-3 font-medium text-lg">Settings</MenuItem>
         <MenuItem icon={<FaQuestion />} className="mt-3 md-3 font-medium text-lg">Help</MenuItem>
         <MenuItem icon={<FaBook />} className="mt-3 md-3 font-medium text-lg">Policies</MenuItem>
         <MenuItem icon={<FaComment />} className="mt-3 md-3 font-medium text-lg">Feedbacks</MenuItem>
 
-        <hr className="w-full h-0.5 bg-red-700"/>
+        <hr className="w-11/12 h-0.5 bg-red-700"/>
 
         <a target="_blanck" href="https://play.google.com/store/search?q=%D8%B1%D8%A7%D8%AF%D9%8A%D9%88+%D8%AB%D9%85%D8%A7%D9%86%D9%8A%D8%A9&c=apps&hl=en">
           <MenuItem icon={<FaArrowUpRightFromSquare />} className="mt-3 md-3 font-medium text-lg">Get the Android app</MenuItem>
